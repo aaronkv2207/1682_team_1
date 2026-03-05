34aa7e8eaa94d08d5b20cd0918d087426c1c4af3
sparke <sparke@mit.edu>
2026-03-04 22:35:07 -0500

    put sizing functions into wing class

--- Python/structural_sizing.py
+++ Python/structural_sizing.py
@@ -1,28 +1,89 @@
 # Team 1 - Structural Sizing
 import numpy as np
+from ... import ... as beam
 
-class Structure:
-    """Gets total structural mass from different structural components"""
-    pass
-
-class Wing(Structure):
+class Wing():
     """Wing class:
     - Takes in general wing sizing and material
     - Sizes structural components on the wing
     - Outputs weight of the wing"""
 
-    def __init__(self, aero_sizing, materials, weight_estimate):
-        self.aero_sizing = aero_sizing
+    # Set constants
+    g = 9.81
+
+    def __init__(self, aero, loading, materials, weight_estimate):
+        self.aero = aero
+        self.loading = loading
         self.material = materials
         self.weight_estimate = weight_estimate
 
-class Tail(Structure):
+    def get_N(self, L, a_z):
+        """Gets flight load factor, landing load factor, and max load factor"""
+        # Get required variable
+        W_total = self.aero["W_total"]
+
+        # Calculate max load factor
+        N = L/W_total # flight load factor
+        N_land = a_z/g # landing load factor
+        N_max = np.max(N, N_land) # maximum load factor
+
+        return N, N_land, N_max
+
+    def spar_cap_area(self):
+        """Find necessary spar cap area based on loading"""
+        # Get required variables
+        c_tip = self.aero["c_tip"]
+        c_0 = self.aero["c_0"]
+        taper_ratio = c_tip/c_0
+        N_max = self.get_N()[2]
+        W_cent = self.aero["W_total"] - self.weight_estimate
+        axial_max = ... # TODO: fill in function from other script
+        b = self.aero["b"]
+        h = self.aero["h"]
+        N = self.get_N()[0]
+        E = self.materials["spar_car_E"] # maybe remain this variable if it shows up again just for clarity
+        w_b_max = self.aero["w_b_max"] # not sure where we'll actually get this
+
+        # Calculate area based on strength and stiffness requirements
+        A_cap_0_strength = (N_max*W_cent)/(12*axial_max)*(b/h)*(1+2*taper_ratio)/(1+taper_ratio)
+        A_cap_0_stiffness = (N*W_cent)/(48*E) * b**2/h**2 * 1/w_b_max * (1+2*taper_ratio)/(1+taper_ratio)
+        return np.max(A_cap_0_strength, A_cap_0_stiffness)
+
+    def spar_web_area(self):
+        """Find necessary spar web area based on loading"""
+        # Get required variables
+        N_max = self.get_N()[2]
+        W_cent = self.aero["W_total"] - self.weight_estimate
+        shear_max = ... # TODO: fill in function from other script
+
+        # Return spar web area
+        return (N_max*W_cent)/(2*shear_max)
+
+    def skin_thickness(self):
+        """Find necessary skin thickness based on loading"""
+
+
+        t_skin_strength = q*b_ail*c_ail**2*c_m*1/(2*A)*1/(1/shear_max)
+        t_skin_stiffness = 1*b_ail*c_ail**2*y_ail*c_m*s_tot/(4*A**2*G)*(1/twist_max)
+
+        return np.max(t_skin_strength, t_skin_stiffness)
+
+    def tube_thickness():
+        # TODO: implement this (not sure where this component goes so not doing this yet)
+        pass
+
+    # add methods for each flight stage that gets the correct loads and then sizes based off that
+    # also add method to run through each flight stage and then get max loading
+    # weight method takes max loading and uses that sizing to get weight of components
+
+
+class Tail():
     pass
 
-class Fuselage(Structure):
+class Fuselage():
     pass
 
-class LandingGear(Structure):
+class LandingGear():
     pass
 
 
