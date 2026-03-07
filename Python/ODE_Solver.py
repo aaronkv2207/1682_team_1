85cfaabd4e25218a6a59947ce8fcef579f4ed120
lillyjohannan <lillyjoh@mit.edu>
2026-03-07 18:04:22 -0500

    more updates to solver

--- Python/ODE_Solver.py
+++ Python/ODE_Solver.py
@@ -4,21 +4,36 @@ import matplotlib.pyplot as plt
 
 
 class Aircraft:
-    def __init__(self, m_total, I_y, rho, S, R, omega, cl, cx, cx_prime, cm, static_condition, h, b, initial_conditions):
+    def __init__(self, m_total, I_y, rho, S, St, R, omega, CL0, CLa,CLq, CL_de, CL_df, cx, cx_prime, Cm0, Cma, Cmq, Cm_de, Cm_df, cd_profile, cd_w, cd_t, CT, static_condition, alpha_0, h, b, c_bar, initial_conditions):
         self.m=m_total
         self.I_y=I_y
         self.S=S
+        self.St=St
         self.R=R
         self.rho=rho
         self.omega=omega
-        self.cl=cl
+        self.CL0=CL0
+        self.CLa=CLa
+        self.CLq=CLq
+        self.CL_de=CL_de
+        self.CL_df=CL_df
         self.cx=cx
         self.cx_prime=cx_prime
-        self.cm=cm
+        self.Cm0=Cm0
+        self.Cma=Cma
+        self.Cmq=Cmq
+        self.Cm_de=Cm_de
+        self.Cm_df=Cm_df
+        self.cd_profile=cd_profile
+        self.cd_w=cd_w
+        self.cd_t=cd_t
+        self.CT=CT
         self.static=static_condition
+        self.alpha_0=alpha_0
         self.h=h
         self.b=b
         self.AR=b**2/S
+        self.c_bar=c_bar
         self.ic=initial_conditions
 
     def aircraft_dynamics(self, t, x):
@@ -26,6 +41,7 @@ class Aircraft:
         x = [u, w, q, theta] state vector
         params = dictionary of aircraft parameters
         """
+        #State vector
         u, w, q, theta, xe, ze = x
 
         # Kinematics
@@ -33,13 +49,17 @@ class Aircraft:
         V = np.sqrt(u**2 + w**2)
         Q = 0.5 * self.rho * V**2
 
+
         # Transform velocities to earth frame
         u_e = u * np.cos(theta) + w * np.sin(theta)
         w_e = w * np.cos(theta) - u * np.sin(theta)
         gamma = np.arctan2(-w_e, u_e)
 
+
         #Forces
         # Thrust / drag
+        #How do I fix this, de coupling T and D to get rid of cx
+        #Are control surfaces inputs into thrust, drag, or both
         if self.static:
             T_D = -Q * self.S * self.cx
         elif ze==0: #if at takeoff, consider phi component on induced drag
@@ -49,7 +69,7 @@ class Aircraft:
             T_D = -0.5 * self.rho * (self.omega * self.R**2) * self.cx_prime
 
         #Lift
-        L=Q*self.S*self.cl
+        L=Q*self.S*self.CL(t, x)
 
         #Rolling resistance at takeoff
         if ze==0:
@@ -65,7 +85,7 @@ class Aircraft:
         Z = T_D * np.sin(alpha) - L * np.cos(alpha)
 
         #Moment
-        M=Q*self.S*(self.cm-.5*q) #added damping term, will go away in real model
+        M=Q*self.S*self.Cm(t, x) #added damping term, will go away in real model
 
 
         # Equations of motion
@@ -81,9 +101,64 @@ class Aircraft:
         u, w =x[0], x[1]
         V=np.sqrt(u**2+w**2)
         Q=0.5*self.rho*V**2
-        L=Q*self.cl*self.S
+        L=Q*self.CL(t, x)*self.S
         W=self.m*9.81
         return L-W
+
+    def CL(self, t, x):
+        #Control surface inputs
+        u, w, q, theta, xe, ze = x
+        alpha = np.arctan2(w, u)
+        ui, wi, qi, thetai, xei, zei, =self.ic
+        Vi=np.sqrt(ui**2+wi**2)
+        q_bari=(qi*self.c_bar)/(2*Vi)
+        V = np.sqrt(u**2 + w**2)
+        q_bar=(q*self.c_bar)/(2*V)
+
+        del_e=self.elevator(t)
+        del_f=self.flaps(t)
+        del_alpha=alpha-self.alpha_0
+        del_qbar=q_bar-q_bari
+
+        return self.CL0+self.CLa*del_alpha+self.CLq*del_qbar+self.CL_df*del_f+self.CL_de*del_e
+
+
+    def Cm(self, t, x):
+        u, w, q, theta, xe, ze = x
+        alpha = np.arctan2(w, u)
+        ui, wi, qi, thetai, xei, zei, =self.ic
+        Vi=np.sqrt(ui**2+wi**2)
+        q_bari=(qi*self.c_bar)/(2*Vi)
+        V = np.sqrt(u**2 + w**2)
+        q_bar=(q*self.c_bar)/(2*V)
+
+        #Include nominal conditions
+        del_e=self.elevator(t)
+        del_f=self.flaps(t)
+        del_alpha=alpha-self.alpha_0
+        del_qbar=q_bar-q_bari
+
+        return self.Cm0+self.Cma*del_alpha+self.Cmq*del_qbar+self.Cm_df*del_f+self.Cm_de*del_e
+    def CD(self, t, x):
+        return self.cd_profile/self.S+self.cd_w+self.cd_t*self.St/self.S+self.CL(t, x)**2/(np.pi*self.AR)
+
+    '''
+    Modeling elevator surface delfection over time.
+    Given a time input, elevator deflection will change, which will go into Cm, CL, etc caclulations'''
+    def elevator(self, t):
+        if t<5:
+            return 0
+        else:
+            return np.radians(-5) #modellig pull up at takeoff after 5 sec
+    def flaps(self, t):
+        if t<20:
+            return np.radians(10)
+        elif t<25:
+            return np.radians(10)*(1-(t-20)/5) #linearly decreasing funnction
+        else:
+            return 0
+
+
 
 Aircraft.takeoff_event.terminal=False # doesn't stop integrating with ivp once takeoff occurs but records when event happens
 Aircraft.takeoff_event.direction=1 #ensures that crossing goes from L-W<0 to L-W>0 to find zero
@@ -106,15 +181,30 @@ plane_1=Aircraft(
     I_y=500,
     rho=1.225,
     S=10,
+    St=...,
     R=2,
     omega=100, #propeller rotation rate
-    cl=0.3,
+    CL0=0.3,
+    CLa= ...,
+    CLq=...,
+    CL_de= ...,
+    CL_df=...,
     cx=0.3, #eventually cx either gets split into compoents or becomes a function  or a function of cl, dt, q_bar, df, Re, Ma
     cx_prime=0.2,
-    cm=0,
+    Cm0=0,
+    Cma=...,
+    Cmq=...,
+    Cm_de=...,
+    Cm_df=...,
+    cd_profile=...,
+    cd_w=...,
+    cd_t=...,
+    CT=...,
     static_condition=False,
+    alpha_0=0,
     h=5.5,
     b=19.8,
+    c_bar=4,#mean aerodynamic cord
     initial_conditions=x0)
 
 #Intergate for 20 seconds
