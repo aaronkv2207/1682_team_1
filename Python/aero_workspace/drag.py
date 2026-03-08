a2db9bf6a21fef452428ad0ad11720660a13204d
Osahon <osahonodiase1234@gmail.com>
2026-03-08 17:03:54 -0400

    lint

--- Python/aero_workspace/drag.py
+++ Python/aero_workspace/drag.py
@@ -128,7 +128,8 @@ def C_Dp(rho, V, mu, l, x_tr):
 
 # = sum(CDA * 1/2 * Vi**3)
 
+
 # calculate induced drag
 def C_Di(C_L):
-    C_Di = C_L**2 / (np.pi*AR*e)
+    C_Di = C_L**2 / (np.pi * AR * e)
     return C_Di
