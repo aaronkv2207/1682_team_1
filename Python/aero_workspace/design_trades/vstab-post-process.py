258e3061318bdd10b2db299e64634975fe06b673
Brenda Chow <brendachow495@gmail.com>
2026-04-06 15:30:05 -0400

    created folders for vstab

---
+++ Python/aero_workspace/design_trades/vstab-post-process.py
@@ -0,0 +1,179 @@
+import os
+import pickle
+import sys
+from dataclasses import dataclass
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pint import UnitRegistry
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from aero_dict import AircraftConfig
+from conceptual_design import ureg
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+from range_model import get_range
+
+DEG2RAD_CONV = ureg("deg").to("rad").magnitude
+METERS2FT_CONV = ureg("m").to("ft").magnitude
+AR = 8
+
+
+def get_runway_length(
+    v=22,
+    S=50,
+    CL=6.1,
+    CD=1.88,
+    mu=0.01,
+    rho=AircraftConfig.rho_t0,
+    T=62000,  # 27496,
+    m=7504,
+    g=9.81,
+):
+    W = m * g
+    L = 0.5 * rho * v**2 * S * CL
+    D = 0.5 * rho * v**2 * S * CD
+
+    net_force = T - D - mu * (W - L)
+
+    # if net_force <= 0:
+    #     raise ValueError("Insufficient thrust to accelerate (denominator <= 0).")
+
+    x_runway = (m * v**2) / (2 * net_force)
+    return x_runway
+
+
+def get_climb_gradient(
+    v,
+    S,
+    CD,
+    rho=AircraftConfig.rho_climb,
+    T=27496,
+    m=7504,
+    g=9.81,
+):
+    W = m * g
+    q = 0.5 * rho * v**2
+    D = q * S * CD
+    gamma = (T - D) / W
+
+    return gamma
+
+
+@dataclass
+class AeroCoeffConfig:
+    """Reads a summary of JVL output dataframe at various operating points. Defines functions
+    based on operating points --> CL, CD, CM. If other parameters are desired, see data dictionary."""
+
+    S: int
+    phase: str  # 'takeoff', 'cruise', or 'landing'
+    name: str  # specify sub-folder
+    max_elevator: float = 20.0  # degrees
+
+    def __post_init__(self):
+        file_path = f"JVL_writer/sref_trades/run_outputs/{self.name}/coeff_results/Sref-{self.S}/{self.phase}.pkl"
+
+        with open(file_path, "rb") as f:
+            data = pickle.load(f)
+
+        self.alphas, self.velocities, self.d_elevator = [], [], []
+        self.CL, self.CD, self.Cm, self.CDind, self.e, self.xto, self.gamma = (
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+        )
+        if self.phase == "cruise":
+            CD_DP = AircraftConfig.C_Dp_cruise
+        else:
+            CD_DP = (
+                AircraftConfig.C_Dp_t0
+            )  # Default for takeoff/landing; may need to modify in future iterations
+
+        for run in data:
+            self.alphas.append(run["alpha"] * DEG2RAD_CONV)
+            self.velocities.append(run["velocity"])
+            self.d_elevator.append(run["d6"])
+            self.CL.append(run["CL"])
+            self.Cm.append(run["Cm"])
+            self.e.append(run["e"])
+
+            _CDind = run["CL"] ** 2 / (AR * np.pi * run["e"])
+            self.CDind.append(_CDind)
+
+            if self.phase in ("takeoff", "landing"):
+                self.xto.append(
+                    get_runway_length(
+                        v=run["velocity"], S=self.S, CL=run["CL"], CD=(CD_DP + _CDind)
+                    )
+                    * METERS2FT_CONV
+                )
+
+        self.CD_tot = (self.CDind + CD_DP) * 1.2
+
+        self.alphas = np.array(self.alphas)
+        self.velocities = np.array(self.velocities)
+        self.d_elevator = np.array(self.d_elevator)
+        self.CL = np.array(self.CL)
+        self.Cm = np.array(self.Cm)
+        self.CDind = np.array(self.CDind)
+        self.e = np.array(self.e)
+
+        # Filter based on reasonable elevator deflections
+        elevator_mask = np.abs(self.d_elevator) < self.max_elevator
+        cl_best_real_mask = np.argmax(self.CL[elevator_mask])
+
+        # produces best - for cl_max
+        if self.phase == "takeoff":
+            self.alphas = self.alphas[cl_best_real_mask]
+            self.velocities = self.velocities[cl_best_real_mask]
+            self.d_elevator = self.d_elevator[cl_best_real_mask]
+            self.CL = self.CL[cl_best_real_mask]
+            self.Cm = self.Cm[cl_best_real_mask]
+            self.CDind = self.CDind[cl_best_real_mask]
+            self.CD_tot = self.CD_tot[cl_best_real_mask]
+            self.e = self.e[cl_best_real_mask]
+            self.xto = np.array(self.xto)[cl_best_real_mask]
+
+        if self.phase == "cruise":  # assuming vcruise of 125 [m/s]
+            self.alphas = self.alphas[1]
+            self.velocities = self.velocities[1]
+            self.d_elevator = self.d_elevator[1]
+            self.CL = self.CL[1]
+            self.Cm = self.Cm[1]
+            self.CDind = self.CDind[1]
+            self.CD_tot = self.CD_tot[1]
+            self.e = self.e[1]
+
+        if self.phase == "climb":
+            # picks best climb condition (max gamma)
+            gamma_all = get_climb_gradient(
+                self.velocities,
+                self.S,
+                self.CD_tot,
+                rho=AircraftConfig.rho_climb,
+                T=27496,  # TODO: update with correct climb thrust
+                m=7504,  # TODO: update with correct climb thrust
+                g=9.81,
+            )
+            self.gamma = gamma_all
+
+            # for steepest climb angle
+            idx = np.argmax(gamma_all)
+            self.alphas_best = self.alphas[idx]
+            self.velocities_best = self.velocities[idx]
+            self.d_elevator_best = self.d_elevator[idx]
+            self.CL_best = self.CL[idx]
+            self.Cm_best = self.Cm[idx]
+            self.CDind_best = self.CDind[idx]
+            self.CD_tot_best = self.CD_tot[idx]
+            self.e_best = self.e[idx]
+            self.gamma_best = self.gamma[idx]
+
+
+if __name__ == "__main__":
+    S_list = np.linspace(40, 52, 13)  # NOTE: See available options in runner script.
